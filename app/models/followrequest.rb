c70f071632ceea767dbd34d3b137efc2d08f199d
Paulina Delmore <delmore.paulina@gmail.com>
2023-03-20 15:16:49 +0000

    add associations

--- app/models/followrequest.rb
+++ app/models/followrequest.rb
@@ -10,4 +10,10 @@
 #  sender_id    :integer
 #
 class Followrequest < ApplicationRecord
+
+  belongs_to :recipient, { :required => true, :class_name => "User", :foreign_key => "recipient_id" }
+
+  belongs_to :sender, { :required => true, :class_name => "User", :foreign_key => "sender_id" }
+
+
 end
